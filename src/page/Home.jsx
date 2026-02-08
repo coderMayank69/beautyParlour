7ab5e8e70d3ed886a9b4d80fb22fbf7f615f92f0
coderMayank.69 <mayankbca96325@gmail.com>
2026-02-08 23:17:11 +0530

    .

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import Hero from '../component/Hero.jsx'
-import About from '../component/About.jsx'
-import Services from '../component/Services.jsx'
-import Testimonial from '../component/Testimonial.jsx'
-import Maps from '../component/Maps.jsx'
+
+import { Suspense, lazy } from 'react';
+
+const Hero = lazy(() => import('../component/Hero.jsx'));
+const About = lazy(() => import('../component/About.jsx'));
+const Services = lazy(() => import('../component/Services.jsx'));
+const Testimonial = lazy(() => import('../component/Testimonial.jsx'));
+const Maps = lazy(() => import('../component/Maps.jsx'));
+import Loader from '../component/Loader.jsx';
+
 
 const Home = () => {
   return (
-    <>
+    <Suspense fallback= {<Loader />}>
       <Hero />
       <About />
       <Services />
       <Testimonial />
       <Maps />
-    </>
-  )
+    </Suspense>
+  );
 }
 
 export default Home
