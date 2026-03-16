18e64f2c18d13b4acb6710ab97e14d63f769e229
coderMayank.69 <mayankbca96325@gmail.com>
2026-03-17 02:58:44 +0530

    Optimized architecture for fast loading, smooth performance, and mobile-friendliness,

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,24 +1,26 @@
 
+
 import { Suspense, lazy } from 'react';
+import Loader from '../component/Loader.jsx';
 
 const Hero = lazy(() => import('../component/Hero.jsx'));
 const About = lazy(() => import('../component/About.jsx'));
 const Services = lazy(() => import('../component/Services.jsx'));
 const Testimonial = lazy(() => import('../component/Testimonial.jsx'));
 const Maps = lazy(() => import('../component/Maps.jsx'));
-import Loader from '../component/Loader.jsx';
-
 
 const Home = () => {
   return (
-    <Suspense fallback= {<Loader />}>
-      <Hero />
-      <About />
-      <Services />
-      <Testimonial />
-      <Maps />
-    </Suspense>
+    <main className="min-h-screen w-full">
+      <Suspense fallback={<Loader />}>
+        <Hero />
+        <About />
+        <Services />
+        <Testimonial />
+        <Maps />
+      </Suspense>
+    </main>
   );
-}
+};
 
-export default Home
+export default Home;
